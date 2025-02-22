5ca98c70821a4db6cf01f735f878e965b4b37586
Ethan Zhao <ethanzhao1981@gmail.com>
2025-02-22 10:09:56 -0500

    reroute workers privkey import

--- workers/workers/connectToDb.py
+++ workers/workers/connectToDb.py
@@ -1,7 +1,6 @@
 from firebase_admin import credentials, firestore, initialize_app
 
-cred = credentials.Certificate("privkey.json")
+cred = credentials.Certificate("../../serviceAccountKey.json")
 initialize_app(cred)
 
 db = firestore.client()
-
