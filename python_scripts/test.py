49583459857626ed497ebb5a5246e8bcff2a7b1d
benjaminnamayandev <benjaminnamayandeh@gmail.com>
2025-02-21 21:46:55 -0500

    Updates to the python file

--- python_scripts/test.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # This loads variables from the .env file into os.environ
-
-api_key = os.environ.get("PERPLEXITY_API_KEY")
-print("Your API key is:", api_key)
