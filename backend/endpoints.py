c250d0cda0e33826b3d9bbbb66189fd1294e16c0
benjaminnamayandev <benjaminnamayandeh@gmail.com>
2025-02-22 09:36:23 -0500

    broke up fastapi endpoints

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -13,30 +13,33 @@ initialize_app(cred)
 db = firestore.client()
 
 @app.get("/data")
-def get_data():
+def get_grocery_items():
     grocery_items = []
-    kitchen_items = []
-    recipes = []
 
     # Retrieve all documents from the "GroceryItems" collection
     grocery_docs = db.collection("GroceryItems").stream()
     for doc in grocery_docs:
         grocery_items.append(doc.to_dict())
 
+    return {"GroceryItems": grocery_items}
+
+@app.get("/data")
+def get_kitchen_items():
+    kitchen_items = []
+
     # Retrieve all documents from the "KitchenItems" collection
     kitchen_docs = db.collection("KitchenItems").stream()
     for doc in kitchen_docs:
         kitchen_items.append(doc.to_dict())
 
+    return {"KitchenItems": kitchen_items}
+
+@app.get("/data")
+def get_recipes():
+    recipes = []
     # Retrieve all documents from the "Recipes" collection
     recipe_docs = db.collection("Recipes").stream()
     for doc in recipe_docs:
         recipes.append(doc.to_dict())
 
-    return {
-        "GroceryItems": grocery_items,
-        "KitchenItems": kitchen_items,
-        "Recipes": recipes
-    }
-
-#print(get_data())
+    return {"Recipes": recipes}
