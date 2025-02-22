af74b5b2062d1d53788fc508501c99743659da95
Zanan Virani <viranizanan@gmail.com>
2025-02-21 21:39:15 -0500

    Tabs created

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,12 +1,12 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from "@/components/HapticTab";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -14,30 +14,62 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            position: "absolute",
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Cart",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={22} name="shopping-cart" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="inventory"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Inventory",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={22} name="inventory" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="suggestions"
+        options={{
+          title: "AI",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={22} name="mic" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="receipt"
+        options={{
+          title: "Recipes",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={22} name="receipt" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="food"
+        options={{
+          title: "Food",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons size={22} name="restaurant" color={color} />
+          ),
         }}
       />
     </Tabs>
---
+++ client/app/(tabs)/suggestions.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const suggestions = () => {
+  return (
+    <View>
+      <Text>suggestions</Text>
+    </View>
+  );
+};
+
+export default suggestions;
