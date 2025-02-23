01b8d8a7462ae3919c1ccc807e3017190e8f8018
benjaminnamayandev <benjaminnamayandeh@gmail.com>
2025-02-23 08:23:57 -0500

    SidePanel done

--- client/components/ShoppingCart/SidePanel.tsx
+++ client/components/ShoppingCart/SidePanel.tsx
@@ -1,5 +1,8 @@
-import { GroceryItem } from "@/types/models";
+import { RootState } from "@/state/store";
+import { GroceryItem, Recipe } from "@/types/models";
+import { useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
 
 interface SidePanelProps {
     item: GroceryItem;
@@ -7,13 +10,37 @@ interface SidePanelProps {
 }
 
 export default function SidePanel({ item, onClose }: SidePanelProps) {
+    const recipes = useSelector((state: RootState) => state.recipeList.recipeList);
+    const [ingredients, setIngredients] = useState<string[]>([]);
+
+    useEffect(() => {
+        if (item.recipe_name) {
+            const recipe = recipes.find((recipe: Recipe) => recipe.name === item.recipe_name);
+            const ingredientList: string[] = [];
+            if (recipe) {
+                for (const ingr of recipe.ingredients_have_per_serving) {
+                    ingredientList.push(ingr.name);
+                }
+                if (recipe.existing_groceries_per_serving) {
+                    for (const ingr of recipe.existing_groceries_per_serving) {
+                        ingredientList.push(ingr.name);
+                    }
+                }
+                if (recipe.new_groceries_per_serving) {
+                    for (const ingr of recipe.new_groceries_per_serving) {
+                        ingredientList.push(ingr.name);
+                    }
+                }
+                setIngredients(ingredientList);
+            }
+        }
+    }, [recipes, item.recipe_name]);
+
     return (
         <View style={styles.panelContainer}>
             <Text style={styles.title}>{item.name}</Text>
 
-            <Text style={styles.itemName}>
-                {item.id ?? "No Item Selected"}
-            </Text>
+            <Text style={styles.itemName}>{item.id}</Text>
 
             {item.ai_reason ? (
                 <View style={styles.blueBox}>
@@ -22,8 +49,8 @@ export default function SidePanel({ item, onClose }: SidePanelProps) {
                             ? "You are running low on this item"
                             : item.ai_reason === "AI_OTHER"
                                 ? "AI recommends this item."
-                                : item.ai_reason === "COMPLETE_RECIPE"
-                                    ? "This completes a recipe."
+                                : item.ai_reason === "COMPLETE_RECIPE" && item.recipe_name
+                                    ? `You are a few ingredients away from ${item.recipe_name}. Missing ingredients: ${ingredients.join(", ")}`
                                     : ""}
                     </Text>
                 </View>
